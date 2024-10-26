35fd8433abc26c7b87cd99e91657dd561be50f90
Eliabe dos Santos Oliveira <72847444+eliabedossantos@users.noreply.github.com>
2024-10-26 20:45:41 -0300

    feat: create user services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService) {}
 
     @Post()
     async createUser(
         @Body() createUserDto: CreateUserDto
-    ) {
-        return {
-            ...createUserDto,
-            password: undefined
-        }
+    ): Promise<User> {
+        return this.userService.createUser(createUserDto)
+    }
+
+    @Get()
+    async getAllUser(): Promise<User[]> {
+        return this.userService.getAllUser();
     }
 }
