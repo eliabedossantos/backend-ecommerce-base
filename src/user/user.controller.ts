b353a0af1adefdb8857ccee6cff9ad50733a9c31
Eliabe dos Santos Oliveira <72847444+eliabedossantos@users.noreply.github.com>
2024-10-29 22:02:08 -0300

    feat: insert address, state and city entities

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(private readonly userService: UserService) {}
 
     @Post()
     async createUser(
